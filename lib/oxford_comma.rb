74baf19d187bf20493311bf9bbb901e6ab8478b2
simokh <m.simokhouili@gmail.com>
2020-04-22 19:47:15 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,13 +1,10 @@
+ def oxford_comma(array)
+  if array.size == 1 
+    array.join(' ')
+    elsif array.size == 2 
+      array.join(" and ")
+    else 
+      array[0..-2].join(", ") + ", and " + array[-1]
+    end
+end 
 
-def oxford_comma(array)
-  if array.length == 1
-    return "#{array[0]}"
-  elsif array.length == 2
-    return array.join(" and ")
-  elsif array.length >= 3
-    new_last_array_item = "and #{array[-1]}"
-    array.pop
-    array.push(new_last_array_item)
-    return array.join(", ")
-  end
-end
